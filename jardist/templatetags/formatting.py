06ad1b2e13d8600ea8049ec0e725ac0e33e7b2cc
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2024-07-02 15:19:17 +0800

    bugfix: currency formatting on linux

--- jardist/templatetags/formatting.py
+++ jardist/templatetags/formatting.py
@@ -6,7 +6,13 @@ register = template.Library()
 
 @register.filter
 def currency(value):
-    locale.setlocale(locale.LC_ALL, 'id_ID.utf8')
+    try:
+        locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
+    except locale.Error:
+        try:
+            locale.setlocale(locale.LC_ALL, 'id_ID.utf8')
+        except locale.Error:
+            pass
     formatted_value = locale.currency(value, grouping=True, symbol='')
     return "Rp. " + formatted_value
 
